5b4992a4387df5d312246818debf9531826353be
Chris Small <mrshrinkray@gmail.com>
2010-04-18 16:30:00 +0100

    controllers

--- Flashback.UI/Controllers/Questions/QuestionsHubController.cs
+++ Flashback.UI/Controllers/Questions/QuestionsHubController.cs
@@ -1,9 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using MonoTouch.UIKit;
 using Flashback.Core;
+using System.IO;
+using System.Reflection;
+using MonoTouch.Foundation;
 
 namespace Flashback.UI.Controllers
 {
@@ -22,6 +25,23 @@ namespace Flashback.UI.Controllers
 		public QuestionsHubController(Category category)
 		{
 			_category = category;
+		}
+		
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			UIWebView webView = new UIWebView();
+			webView.Frame = new System.Drawing.RectangleF(0,0,380,420);
+			
+			string template = "";
+			Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Flashback.Assets.HTML.calendar.html");
+			using (StreamReader reader = new StreamReader(stream))
+			{
+				template = reader.ReadToEnd();
+			}
+			webView.LoadHtmlString(template,new NSUrl("/"));
+			View.AddSubview(webView);
 		}
 	}
 }
