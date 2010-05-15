cc1a1210c8e294d307ab7c344498ce70282b89ef
Chris Small <mrshrinkray@gmail.com>
2010-05-15 18:49:16 +0100

    Mac: Missing images/source from forgeting addremove

---
+++ Flashback.UI/BusyView.cs
@@ -0,0 +1,28 @@
+using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+
+namespace Flashback
+{
+	public class BusyView : UIAlertView
+	{
+		private UIActivityIndicatorView _activityView;
+
+		public void Show(string title)
+		{
+			Title = title;
+			Show();
+
+			// Spinner - add after Show() or we have no Bounds.
+			_activityView = new UIActivityIndicatorView(UIActivityIndicatorViewStyle.WhiteLarge);
+			_activityView.Frame = new RectangleF((Bounds.Width / 2) - 15, Bounds.Height - 50, 30, 30);
+			_activityView.StartAnimating();
+			AddSubview(_activityView);
+		}
+
+		public void Hide()
+		{
+			DismissWithClickedButtonIndex(0, true);
+		}
+	}
+}
