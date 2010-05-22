bcee17ae9ba3ab042d35c59330d249af00323d87
Chris Small <mrshrinkray@gmail.com>
2010-05-23 00:13:52 +0100

    Mac: missing addremove

---
+++ Flashback.UI/Views/UnderlineButton.cs
@@ -0,0 +1,23 @@
+using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+using MonoTouch.CoreGraphics;
+
+namespace Flashback.UI
+{
+	public class UnderlineButton : UIButton
+	{		
+		public override void Draw(RectangleF rect)
+		{
+			base.Draw (rect);
+			
+			// Red underline
+			CGContext context = UIGraphics.GetCurrentContext();
+			context.SetRGBFillColor(0xFF,0xFF,0,1);
+			context.SetLineWidth(1);
+			context.MoveTo(0,rect.Height -5);
+			context.AddLineToPoint(rect.Width,rect.Height -5);
+			context.StrokePath();	
+		}
+	}
+}
