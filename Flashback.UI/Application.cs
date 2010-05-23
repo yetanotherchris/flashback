4bad02cfc5c813f695427002db815edaee422925
Chris Small <mrshrinkray@gmail.com>
2010-05-23 22:48:24 +0100

    PC: Tip/help HTML

--- Flashback.UI/Application.cs
+++ Flashback.UI/Application.cs
@@ -10,6 +10,8 @@ namespace Flashback.UI
 {
 	public class Application
 	{
+		public static string UpgradeLink = "http://phobos.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=366862216";
+
 		public static void Main(string[] args)
 		{
 			UIApplication.Main(args,null,"AppDelegate");
@@ -20,7 +22,7 @@ namespace Flashback.UI
 		/// </summary>
 		public static void LaunchAppstoreProEdition()
 		{
-			UIApplication.SharedApplication.OpenUrl(new NSUrl("http://phobos.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=301349397"));
+			UIApplication.SharedApplication.OpenUrl(new NSUrl(UpgradeLink));
 		}
 	}
 }
